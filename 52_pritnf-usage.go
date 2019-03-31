0507ebc8a8206936703dcb8a437411900bbe545c
okumurakengo <okumurakengo1a@gmail.com>
2019-03-31 21:25:54 +0900

    pritf、エラー追加

---
+++ 52_pritnf-usage.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("Binary: %b\\%b\n", 4, 5)
+    fmt.Printf("%d %%\n", 50)
+}
---
+++ fmt-error.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("Binary: %b\\%b", 4)
+}
